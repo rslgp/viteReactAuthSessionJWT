a5fb5f8ba40ec79529d9d55dcaf956546810f2e9
Rafael Leao <rslgp@cin.ufpe.br>
2025-02-06 20:58:19 -0300

    fix ghpages vite

--- frontend_react_viteJWT/vite.config.js
+++ frontend_react_viteJWT/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/viteReactAuthSessionJWT/',
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001', // Your backend URL
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:3001', // Your backend URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional
       },
