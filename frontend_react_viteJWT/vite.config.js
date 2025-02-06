ba34d7041d658f0a7dd4ae72171fe26aa1998cab
Rafael Leao <rslgp@cin.ufpe.br>
2025-02-06 20:59:05 -0300

    hide backend url endpoint + ghpages

--- frontend_react_viteJWT/vite.config.js
+++ frontend_react_viteJWT/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/viteReactAuthSessionJWT/',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:3001', // Your backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional
+      },
+    },
+  },
 })
